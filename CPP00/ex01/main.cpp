74cb4aef33ca9fb5e44f2d0ad593cca4626810f4
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-01-31 18:23:23 +0100

    makefile

---
+++ CPP00/ex01/main.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phone_book.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/31 17:57:57 by anfouger          #+#    #+#             */
+/*   Updated: 2026/01/31 18:10:59 by anfouger         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main(void)
+{
+	std::string input;
+	std::getline(std::cin, input);
+	std::cout << input;
+	return (0);
+}
