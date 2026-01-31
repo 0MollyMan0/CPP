8f1ed232202e4c011bdd775c4142cbc47546b277
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-01-31 18:30:22 +0100

    end of day

--- CPP00/ex01/command.cpp
+++ CPP00/ex01/command.cpp
@@ -1,12 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   command.c                                          :+:      :+:    :+:   */
+/*   command.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 18:11:40 by anfouger          #+#    #+#             */
-/*   Updated: 2026/01/31 18:11:41 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/01/31 18:29:06 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <phone_book.hpp>
+
+void	command(void)
+{
+	std::string input;
+	std::getline(std::cin, input);
+	std::cout << input;
+}
--- CPP00/ex01/phone_book.hpp
+++ CPP00/ex01/phone_book.hpp
@@ -1,19 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   phone_book.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/31 17:57:57 by anfouger          #+#    #+#             */
-/*   Updated: 2026/01/31 18:29:22 by anfouger         ###   ########.fr       */
+/*   Created: 2026/01/31 18:26:10 by anfouger          #+#    #+#             */
+/*   Updated: 2026/01/31 18:29:12 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <phone_book.hpp>
+#ifndef PHONE_BOOK
+# define PHONE_BOOK
 
-int main(void)
-{
-	command();
-	return (0);
-}
+# include <iostream>
+# include <string>
+# include <cctype>
+
+void	command(void);
+
+#endif
