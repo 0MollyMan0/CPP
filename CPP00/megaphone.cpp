291125335301c3eee3d285e0e19279a3398e9304
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-01-31 17:54:25 +0100

    first cpp exo done

--- CPP00/megaphone.cpp
+++ CPP00/megaphone.cpp
@@ -1,12 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 13:23:00 by anfouger          #+#    #+#             */
-/*   Updated: 2026/01/31 13:23:01 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/01/31 17:53:46 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <cctype>
+
+int	main(int ac, char *av[])
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return 0;
+	}
+
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; av[i][j]; j++)
+			std::cout << (char)std::toupper(av[i][j]);
+	}
+	std::cout << '\n';
+	return 0;
+}
