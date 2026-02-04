4e1d2d9ddc381c0515480e12dca4aa778da5cd98
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-02-04 15:50:37 +0100

    end of day (CPP00 - PhoneBook)

--- CPP00/ex01/PhoneBook.cpp
+++ CPP00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 10:18:48 by anfouger          #+#    #+#             */
-/*   Updated: 2026/02/04 14:30:39 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/02/04 15:48:18 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,51 @@
 PhoneBook::PhoneBook() 
 {
 	newIndex = 0;
+	numberContacts = 0;
+}
+
+void PhoneBook::printContact(int index)
+{
+	std::cout << contacts[index].getFirstName() << "\n";
+	std::cout << contacts[index].getLastName() << "\n";
+	std::cout << contacts[index].getNickname() << "\n";
+	std::cout << contacts[index].getPhoneNumber() << "\n";
+	std::cout << contacts[index].getDarkestSecret() << "\n";
+}
+
+void PhoneBook::printAllContact()
+{
+	int index = 0;
+	std::string value;
+	
+	std::cout << "Index     |First Name|Last name |Nickname  ";
+	while (!contacts[index].getFirstName().empty())
+	{
+		std::cout << index << "         |";
+		for (size_t j = 0; j < 3; j++)
+		{
+			if (j == 0)
+				value = contacts[index].getFirstName();
+			else if (j == 1)
+				value = contacts[index].getLastName();
+			else if (j == 2)
+				value = contacts[index].getNickname();
+			for (size_t i = 0; i < 10; i++)
+			{
+				if (value[i])
+				{
+					if (value[10] && i == 9)
+						std::cout << ".";
+					else
+						std::cout << value[i];
+				}
+				else
+					std::cout << " ";
+			}
+			std::cout << "|";
+		}
+		index++;	
+	}
 }
 
 bool PhoneBook::addContact()
@@ -76,14 +121,23 @@ bool PhoneBook::addContact()
 	contacts[newIndex].setDarkestSecret(input);
 
 	newIndex++;
+	if (numberContacts != 8)
+		numberContacts++;
 	return (true);
 }
 
-void PhoneBook::printContact(int index)
+void PhoneBook::searchContact()
 {
-	std::cout << contacts[index].getFirstName() << "\n";
-	std::cout << contacts[index].getLastName() << "\n";
-	std::cout << contacts[index].getNickname() << "\n";
-	std::cout << contacts[index].getPhoneNumber() << "\n";
-	std::cout << contacts[index].getDarkestSecret() << "\n";
+	std::string input;
+	
+	printAllContact();
+	std::cout << "Enter the index of the contact you wanna see: ";
+	std::getline(std::cin, input);
+	while (!s || input < "0" || input > numberContacts)
+	{
+		std::cout << "A valid index is an index between " << ;
+		std::cout << "Enter the index of the contact you wanna see: ";
+		std::getline(std::cin, input);
+	}
+	
 }
--- CPP00/ex01/PhoneBook.hpp
+++ CPP00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 10:18:39 by anfouger          #+#    #+#             */
-/*   Updated: 2026/02/04 14:11:28 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/02/04 15:41:19 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,18 @@
 
 class PhoneBook
 {
-private:
-	Contact contacts[8];
-	int	newIndex;
-public:
-	PhoneBook();
-	bool addContact();
-	void printContact(int index);
+	private:
+		Contact contacts[8];
+		int	newIndex;
+		int numberContacts;
+		
+		void printContact(int index);
+		void printAllContact();
+		
+	public:
+		PhoneBook();
+		bool addContact();
+		void searchContact();
 };
 
 #endif
