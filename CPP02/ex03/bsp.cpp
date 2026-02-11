0d9d20046ecba2e10587249d593817479831b795
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-02-11 12:07:29 +0100

    bsp done

--- CPP02/ex03/bsp.cpp
+++ CPP02/ex03/bsp.cpp
@@ -6,11 +6,19 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 10:44:05 by anfouger          #+#    #+#             */
-/*   Updated: 2026/02/11 11:49:31 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/02/11 12:05:47 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./Point.hpp"
+
+static bool is_same(Point const a, Point const b)
+{
+	if (a.getX() == b.getX() && a.getY() == b.getY())
+		return (true);
+	else
+		return (false);	
+}
 
 static Fixed cal_w1(Point const a, Point const b, Point const c, Point const point)
 {
@@ -24,7 +32,29 @@ static Fixed cal_w1(Point const a, Point const b, Point const c, Point const poi
 	return (res); 
 }
 
-bool bsp(Point const a, Point const b, Point const c, Point const point)
+static Fixed cal_w2(Point const a, Point const b, Point const c, Point const point, Fixed const w1)
 {
+	Fixed numerator;
+	Fixed denominator;
+	Fixed res;
 	
+	numerator = point.getY() - a.getY() - w1 * (b.getY() - a.getY());
+	denominator = c.getY() - a.getY();
+	res = numerator / denominator;
+	return (res); 
+}
+
+
+
+bool bsp(Point const a, Point const b, Point const c, Point const point)
+{	
+	if (is_same(a, point) || is_same(b, point) || is_same(c, point))
+		return (false);
+
+	Fixed const w1 = cal_w1(a, b, c, point);
+	Fixed const w2 = cal_w2(a, b, c, point, w1);
+	if (w1 > 0 && w2 > 0 && (w1 + w2) < 1)
+		return (true);
+	else
+		return (false);
 }
--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -6,27 +6,21 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/10 09:15:02 by anfouger          #+#    #+#             */
-/*   Updated: 2026/02/10 12:55:32 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/02/11 12:07:10 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./Fixed.hpp"
+#include "./Point.hpp"
+#include "./bsp.cpp"
 #include <iostream>
 
-int main( void ) 
+static parsing()
 {
-	Fixed a;
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 	
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	
-	std::cout << b << std::endl;
-	
-	std::cout << Fixed::max( a, b ) << std::endl;
+}
+
+int main(int ac, char **av) 
+{
 	
 	return 0;
 }
