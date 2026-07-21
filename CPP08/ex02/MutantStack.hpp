8f960466a70113e2c38216c92d629156780a91c2
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-07-22 00:51:05 +0200

    finish ex02

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -6,15 +6,29 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/21 21:52:08 by anfouger          #+#    #+#             */
-/*   Updated: 2026/07/21 21:53:54 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/07/22 00:48:50 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
+# include <stack>
 
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+private:
+public:
+	typedef typename std::stack<T>::container_type::iterator iterator;
 
-
-# include "MutantStack.tpp"
+	iterator begin()
+	{
+		return (this->c.begin());
+	}
+	iterator end()
+	{
+		return (this->c.end());
+	}
+};
 
 #endif
