c253d27c18bb6db0ab45473e2d8a5cf42d27dc59
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-07-21 05:42:23 +0200

    finish ex01

--- CPP08/ex01/Span.cpp
+++ CPP08/ex01/Span.cpp
@@ -6,9 +6,72 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/21 03:18:49 by anfouger          #+#    #+#             */
-/*   Updated: 2026/07/21 03:18:57 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/07/21 05:41:23 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
+Span::Span(unsigned int N)
+{
+	this->_maxSize = N;
+}
+
+Span::Span(const Span& og)
+{
+	this->_container = og._container;
+	this->_maxSize = og._maxSize;
+}
+
+Span::~Span()
+{
+}
+
+void			Span::addNumber(int nb)
+{
+	if (this->_container.size() >= this->_maxSize)
+		throw std::runtime_error("Class is full");
+	else
+		this->_container.push_back(nb);
+}
+
+unsigned int	Span::shortestSpan(void)
+{
+	if (this->_container.size() <= 1)
+		throw std::runtime_error("Not enougth number in class");
+
+	std::vector<int>	sorted = this->_container;
+	std::sort(sorted.begin(), sorted.end());
+	std::vector<int>::iterator it = sorted.begin();
+	unsigned int		shortest;
+	unsigned int		tmp;
+	
+	shortest = *(it + 1) - *it;
+	while (it + 1 != sorted.end())
+	{
+		tmp = *(it + 1) - *it;
+		if (tmp < shortest)
+			shortest = tmp;
+		++it;
+	}
+	return (shortest);
+}
+
+Span& Span::operator=(const Span& og)
+{
+	if (this != &og)
+	{
+		this->_container = og._container;
+		this->_maxSize = og._maxSize;
+	}
+	return (*this);
+}
+
+unsigned int	Span::longestSpan(void)
+{
+	if (this->_container.size() <= 1)
+		throw std::runtime_error("Not enougth number in class");
+	std::vector<int>::iterator maxIt = std::max_element(this->_container.begin(), this->_container.end());
+	std::vector<int>::iterator minIt = std::min_element(this->_container.begin(), this->_container.end());
+	return (*maxIt - *minIt);
+}
--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -6,14 +6,141 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/21 03:17:38 by anfouger          #+#    #+#             */
-/*   Updated: 2026/07/21 03:19:04 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/07/21 05:34:41 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
-int main(int argc, char const *argv[])
+#include <iostream>
+#include <vector>
+#include <list>
+#include <cstdlib>
+#include <ctime>
+
+static void separator(const std::string& title)
 {
-	
-	return 0;
+	std::cout << "\n========== " << title << " ==========\n";
+}
+
+int main(void)
+{
+	srand(time(NULL));
+
+	separator("Subject Test");
+
+	try
+	{
+		Span sp(5);
+
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+
+		std::cout << "Shortest span : " << sp.shortestSpan() << std::endl;
+		std::cout << "Longest span  : " << sp.longestSpan() << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	separator("Overflow");
+
+	try
+	{
+		Span sp(3);
+
+		sp.addNumber(1);
+		sp.addNumber(2);
+		sp.addNumber(3);
+
+		std::cout << "Adding one more..." << std::endl;
+		sp.addNumber(4);
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << "Exception : " << e.what() << std::endl;
+	}
+
+	separator("Not enough numbers");
+
+	try
+	{
+		Span sp(10);
+
+		sp.addNumber(42);
+
+		std::cout << sp.shortestSpan() << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << "Exception : " << e.what() << std::endl;
+	}
+
+	separator("Range using vector");
+
+	try
+	{
+		std::vector<int> values;
+
+		for (int i = 0; i < 10; i++)
+			values.push_back(i * 5);
+
+		Span sp(10);
+
+		sp.addNumbers(values.begin(), values.end());
+
+		std::cout << "Shortest span : " << sp.shortestSpan() << std::endl;
+		std::cout << "Longest span  : " << sp.longestSpan() << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	separator("Range using list");
+
+	try
+	{
+		std::list<int> values;
+
+		values.push_back(8);
+		values.push_back(15);
+		values.push_back(42);
+		values.push_back(100);
+		values.push_back(101);
+
+		Span sp(5);
+
+		sp.addNumbers(values.begin(), values.end());
+
+		std::cout << "Shortest span : " << sp.shortestSpan() << std::endl;
+		std::cout << "Longest span  : " << sp.longestSpan() << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	separator("10000 Numbers");
+
+	try
+	{
+		Span sp(10000);
+
+		for (int i = 0; i < 10000; i++)
+			sp.addNumber(rand());
+
+		std::cout << "Shortest span : " << sp.shortestSpan() << std::endl;
+		std::cout << "Longest span  : " << sp.longestSpan() << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	return (0);
 }
