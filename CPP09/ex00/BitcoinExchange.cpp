6053dd9eb67d6175c37405f5c7e9b93a9924b65d
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-08-03 16:14:40 +0200

    making up the class

--- CPP09/ex00/BitcoinExchange.cpp
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -6,8 +6,53 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/03 15:03:04 by anfouger          #+#    #+#             */
-/*   Updated: 2026/08/03 15:03:12 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/08/03 16:14:14 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <BitcoinExchange.hpp>
+
+/* --- Private Functions --- */
+
+bool	BitcoinExchange::isValidDate(std::string date)
+{
+	(void)date;
+	return (false);
+}
+
+bool	BitcoinExchange::isValidValue(double value)
+{
+	(void)value;
+	return (false);
+}
+
+/* --- Public Functions --- */
+BitcoinExchange::BitcoinExchange()
+{
+}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& other)
+{
+	this->_database = other._database;
+}
+
+BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other)
+{
+	if (this != &other)
+		this->_database = other._database;
+	return *this;
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+}
+
+void BitcoinExchange::loadDatabase(std::string filename)
+{
+	(void)filename;
+}
+
+void BitcoinExchange::processInput(std::string filename)
+{
+	(void)filename;	
+}
--- CPP09/ex00/BitcoinExchange.hpp
+++ CPP09/ex00/BitcoinExchange.hpp
@@ -6,14 +6,31 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/03 15:02:27 by anfouger          #+#    #+#             */
-/*   Updated: 2026/08/03 15:02:59 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/08/03 16:12:12 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
+# include <stdlib.h>
+# include <map>
+# include <string>
 
+class BitcoinExchange
+{
+private:
+	std::map<std::string, double> _database;
 
+	bool isValidDate(std::string date);
+	bool isValidValue(double value);
+public:
+	BitcoinExchange();
+	BitcoinExchange(const BitcoinExchange& other);
+	BitcoinExchange& operator=(const BitcoinExchange& other);
+	~BitcoinExchange();
 
+	void loadDatabase(std::string filename);
+	void processInput(std::string filename);
+};
 
 #endif
