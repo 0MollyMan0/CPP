dace278e689a479570205c13ba8962769394ded1
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-08-10 23:54:28 +0200

    add canonical form

--- CPP09/ex01/RPN.cpp
+++ CPP09/ex01/RPN.cpp
@@ -6,9 +6,29 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/10 23:31:29 by anfouger          #+#    #+#             */
-/*   Updated: 2026/08/10 23:31:44 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/08/10 23:54:03 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
+
+RPN::RPN()
+{
+}
+
+RPN::RPN(const RPN& other)
+{
+	this->_stack = other._stack;
+}
+
+RPN& RPN::operator=(const RPN& other)
+{
+	if (this != &other)
+		this->_stack = other._stack;
+	return (*this);
+}
+
+RPN::~RPN()
+{
+}
 
--- CPP09/ex01/RPN.hpp
+++ CPP09/ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/10 23:31:02 by anfouger          #+#    #+#             */
-/*   Updated: 2026/08/10 23:45:07 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/08/10 23:51:49 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,25 @@
 # include <string>
 # include <iostream>
 
+# define RESET "\033[0m"
+# define BLACK "\033[1;30m"
+# define RED "\033[1;31m"
+# define GREEN "\033[1;32m"
+# define YELLOW "\033[1;33m"
+# define BLUE "\033[1;34m"
+# define MAGENTA "\033[1;35m"
+# define CYAN "\033[1;36m"
+# define WHITE "\033[1;37m"
+
 class RPN
 {
 private:
-	
+	std::stack<int> _stack;
 public:
 	RPN();
+	RPN(const RPN& other);
+	RPN& operator=(const RPN& other);
 	~RPN();
 };
-
-RPN::RPN()
-{
-}
-
-RPN::~RPN()
-{
-}
-
 
 #endif
