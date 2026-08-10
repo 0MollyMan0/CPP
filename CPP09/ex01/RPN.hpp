14cec2e5c58f5d7a491e1684da2bf48718746b20
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-08-10 23:49:30 +0200

    begin ex01

---
+++ CPP09/ex01/RPN.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RPN.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/10 23:31:02 by anfouger          #+#    #+#             */
+/*   Updated: 2026/08/10 23:45:07 by anfouger         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RPN_HPP
+# define RPN_HPP
+
+# include <stdlib.h>
+# include <stack>
+# include <string>
+# include <iostream>
+
+class RPN
+{
+private:
+	
+public:
+	RPN();
+	~RPN();
+};
+
+RPN::RPN()
+{
+}
+
+RPN::~RPN()
+{
+}
+
+
+#endif
