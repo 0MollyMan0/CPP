4d3da5e27655bd75c6fd09392e937b639f13b52c
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-08-11 02:15:51 +0200

    fix some bullshit

--- CPP06/ex01/Serializer.hpp
+++ CPP06/ex01/Serializer.hpp
@@ -6,7 +6,7 @@
 /*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/12 14:04:50 by anfouger          #+#    #+#             */
-/*   Updated: 2026/07/12 15:20:15 by anfouger         ###   ########.fr       */
+/*   Updated: 2026/08/09 19:15:52 by anfouger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ class Serializer
 private:
 	Serializer();
 	~Serializer();
-	Serializer(const ScalarConverter& other);
-	Serializer& operator=(const ScalarConverter& other);
+	Serializer(const Serializer& other);
+	Serializer& operator=(const Serializer& other);
 public:
 	static uintptr_t serialize(Data* ptr);
 	static Data* deserialize(uintptr_t raw);
