fdf4ea5493d0d4783b55417eadd220dc6e6b9d66
0MollyMan0 <fougereantoine0pro@gmail.com>
2026-08-11 21:48:59 +0200

    begin ex02

---
+++ CPP09/ex02/PmergeMe.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anfouger <anfouger@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/11 21:47:05 by anfouger          #+#    #+#             */
+/*   Updated: 2026/08/11 21:48:22 by anfouger         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+# define RESET "\033[0m"
+# define BLACK "\033[1;30m"
+# define RED "\033[1;31m"
+# define GREEN "\033[1;32m"
+# define YELLOW "\033[1;33m"
+# define BLUE "\033[1;34m"
+# define MAGENTA "\033[1;35m"
+# define CYAN "\033[1;36m"
+# define WHITE "\033[1;37m"
+
+#include <stdlib.h>
+
+
+class PmergeMe
+{
+private:
+	
+public:
+	PmergeMe();
+	~PmergeMe();
+};
+
+
+#endif
